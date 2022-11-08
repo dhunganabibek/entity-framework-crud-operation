cb325c79beed5a91ad1ff553e7f33ddccf7ab5a1
Bibek Dhungana <bibek.dhungana@hcss.com>
2022-11-08 17:17:57 -0600

    basic dotnet core web MVV application template

---
+++ entity-framework-crud-operation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace entity_framework_crud_operation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
