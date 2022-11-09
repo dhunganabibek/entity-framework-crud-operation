396b783e393f2fab49f017c0b4ea2c1a30d1402e
Bibek Dhungana <bibek.dhungana@hcss.com>
2022-11-08 18:28:04 -0600

    Model classes added

---
+++ entity-framework-crud-operation/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace entity_framework_crud_operation.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+
+        public bool IsActive { get; set; }  
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+    }
+
+}
